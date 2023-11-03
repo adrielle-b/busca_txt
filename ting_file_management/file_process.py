dc11f1455161ec98aa00823fbb778f141139cc50
adrielle-b <adriielle@gmail.com>
2023-11-03 12:04:36 -0300

    requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+import sys
 
+
+def process(path_file, instance):
+    file_list = [
+        instance.search(i)['nome_do_arquivo'] for i in range(len(instance))
+    ]
+
+    if path_file not in file_list:
+        lines = txt_importer(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(data)
+        print(data, file=sys.stdout)
 
 def remove(instance):
     """Aqui irá sua implementação"""
