ef7d0ce9f1958b3f3adeb24fc212aa76bb212725
adrielle_b <adrielle_braga@hotmail.com>
2023-11-06 18:46:14 -0300

    lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,7 +4,7 @@ def exists_word(word, instance):
     for index in instance._queue:
         ocurrences = []
 
-        for line_number, line in enumerate(index["linhas_do_arquivo"],1):
+        for line_number, line in enumerate(index["linhas_do_arquivo"], 1):
             if word.lower() in line.lower():
                 ocurrences.append({"linha": line_number})
 
@@ -19,7 +19,7 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     result_search = []
-    
+
     for index in instance._queue:
         ocurrences = []
 
