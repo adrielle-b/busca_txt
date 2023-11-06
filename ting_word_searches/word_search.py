d29b09c348369d36e5578c52a11fec9ce183cb35
adrielle_b <adrielle_braga@hotmail.com>
2023-11-06 18:37:34 -0300

    requisitos 07 e 08

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_search = []
+
+    for index in instance._queue:
+        ocurrences = []
+
+        for line_number, line in enumerate(index["linhas_do_arquivo"],1):
+            if word.lower() in line.lower():
+                ocurrences.append({"linha": line_number})
+
+        if ocurrences:
+            result_search.append({
+                "palavra": word,
+                "arquivo": index['nome_do_arquivo'],
+                "ocorrencias": ocurrences
+            })
+    return result_search
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_search = []
+    
+    for index in instance._queue:
+        ocurrences = []
+
+        for line_number, line in enumerate(index["linhas_do_arquivo"], 1):
+            if word.lower() in line.lower():
+                ocurrences.append({"linha": line_number, "conteudo": line})
+
+        if ocurrences:
+            result_search.append({
+                    "palavra": word,
+                    "arquivo": index["nome_do_arquivo"],
+                    "ocorrencias": ocurrences,
+                })
+    return result_search
